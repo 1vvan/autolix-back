c664c1b6f1979fc164a5060a0af3d72db9181afe
Ivan <123951392+1vvan@users.noreply.github.com>
2024-04-21 21:26:06 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'autodealer',
-  password: 'admin',
+  user: 'upu5i5g3317pn',
+  host: 'c6b7lkfdshud3i.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
+  database: 'd5fflrr95enrt9',
+  password: 'p7f789680a5e1f5708049de63a355ea5730e0af858ce793db8cfe4f69368d1d0c',
   port: 5432,
 });
 
