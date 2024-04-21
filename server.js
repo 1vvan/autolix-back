e027aeb7013349b9ad9c97d8e172c0ededcb8011
Ivan <123951392+1vvan@users.noreply.github.com>
2024-04-21 21:30:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const modelsRouter = require('./routes/models.router')
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://autolix.vercel.app/',
     optionSuccessStatus: 200,
 }
 
