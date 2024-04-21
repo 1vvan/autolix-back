2499680e91315f86ac814ac23ff7ccc564a7bbea
Ivan <123951392+1vvan@users.noreply.github.com>
2024-04-22 01:58:33 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const { Pool } = require('pg');
+
+const isProduction = process.env.CONFIG === 'production';
+
 const pool = new Pool({
-  user: 'upu5i5g3317pn',
-  host: 'c6b7lkfdshud3i.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
-  database: 'd5fflrr95enrt9',
-  password: 'p7f789680a5e1f5708049de63a355ea5730e0af858ce793db8cfe4f69368d1d0c',
-  port: 5432,
-  ssl: {
-        rejectUnauthorized: false
-    }
+  user: isProduction ? process.env.PROD_DB_USER : process.env.LOCAL_DB_USER,
+  host: isProduction ? process.env.PROD_DB_HOST : process.env.LOCAL_DB_HOST,
+  database: isProduction ? process.env.PROD_DB_DATABASE : process.env.LOCAL_DB_DATABASE,
+  password: isProduction ? process.env.PROD_DB_PASSWORD : process.env.LOCAL_DB_PASSWORD,
+  port: isProduction ? process.env.PROD_DB_PORT : process.env.LOCAL_DB_PORT,
+  ssl: isProduction ? { rejectUnauthorized: false } : undefined
 });
 
 module.exports = pool;
