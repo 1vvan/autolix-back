b0a6de294916707b5135ef9cc78eb29c1b4912b7
Ivan <123951392+1vvan@users.noreply.github.com>
2024-04-21 21:36:29 +0300

    Update db.js

--- db.js
+++ db.js
@@ -5,6 +5,9 @@ const pool = new Pool({
   database: 'd5fflrr95enrt9',
   password: 'p7f789680a5e1f5708049de63a355ea5730e0af858ce793db8cfe4f69368d1d0c',
   port: 5432,
+  ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
